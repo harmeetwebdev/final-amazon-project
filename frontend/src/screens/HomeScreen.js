3f8c2f12806669991907313e2c52cc9fcf2947d4
Harmeet Bansal <harmeets627@gmail.com>
2022-11-11 16:58:42 +0530

    video-05

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,21 +1,39 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { Link } from "react-router-dom"
 // import data from "../data"
 import axios from 'axios'
+import logger from "use-reducer-logger"
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'FETCH_REQUEST':
+            return { ...state, loading: true };
+        case 'FETCH_SUCCESS':
+            return { ...state, products: action.payload, loading: false }
+        case 'FETCH_FAIL':
+            return { ...state, loading: false, error: action.payload }
+        default:
+            return state;
+    }
+}
 
 function HomeScreen() {
-    const [products, setProducts] = useState([]);
+    const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+        products: [],
+        loading: true,
+        error: '',
+    });
+    // const [products, setProducts] = useState([]);
     useEffect(() => {
         const fetchData = async () => {
-            // dispatch({ type: 'FETCH_REQUEST' });
-            // try {
-            const result = await axios.get('/api/products');
-            //     dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
-            // } catch (err) {
-            //     dispatch({ type: 'FETCH_FAIL', payload: err.message })
-            // }
+            dispatch({ type: 'FETCH_REQUEST' });
+            try {
+                const result = await axios.get('/api/products');
+                dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
+            } catch (err) {
+                dispatch({ type: 'FETCH_FAIL', payload: err.message })
+            }
 
-            setProducts(result.data)
+            // setProducts(result.data)
         };
         fetchData();
     }, []);
@@ -23,20 +41,25 @@ function HomeScreen() {
     return <div>
         <h1>Featured product</h1>
         <div className='products'>
-            {products.map(product => (
-                < div className='product' key={product.slug}>
-                    <Link to={`/product/${product.slug}`}>
-                        <img src={product.image} alt={product.name} />
-                    </Link>
-                    <div className='product-info'>
+            {loading ? (
+                <div>Loading...</div>
+            ) : error ? (
+                <div>{error}</div>
+            ) : (
+                products.map(product => (
+                    < div className='product' key={product.slug}>
                         <Link to={`/product/${product.slug}`}>
-                            <p>{product.name}</p>
+                            <img src={product.image} alt={product.name} />
                         </Link>
-                        <p><strong>${product.price}</strong></p>
-                        <button>Add to Cart</button>
+                        <div className='product-info'>
+                            <Link to={`/product/${product.slug}`}>
+                                <p>{product.name}</p>
+                            </Link>
+                            <p><strong>${product.price}</strong></p>
+                            <button>Add to Cart</button>
+                        </div>
                     </div>
-                </div>
-            ))}
+                )))}
         </div>
     </div>
 }
