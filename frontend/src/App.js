133ab14366120b67a405ea31e6522be4defff634
Harmeet Bansal <harmeets627@gmail.com>
2022-11-11 16:19:43 +0530

    video-02

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,22 @@
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import data from './data';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <header>
-          <a href='/'>amazone</a>
+          <Link to='/'>amazone</Link>
         </header>
         <main>
-
-          <h1>Featured product</h1>
-          <div className='products'>
-            {data.products.map(product => (
-              < div className='product' key={product.slug}>
-                <a href={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </a>
-                <div className='product-info'>
-                  <a href={`/product/${product.slug}`}>
-                    <p>{product.name}</p>
-                  </a>
-                  <p><strong>${product.price}</strong></p>
-                  <button>Add to Cart</button>
-                </div>
-              </div>
-            ))}
-          </div>
+          <Routes>
+            <Route path='/product/:slug' element={<ProductScreen />} />
+            <Route path='/' element={<HomeScreen />} />
+          </Routes>
         </main >
       </div>
     </BrowserRouter >
