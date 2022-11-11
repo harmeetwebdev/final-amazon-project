a48d3c7a9c5b59bf40b8002dfab98a384989a134
Harmeet Bansal <harmeets627@gmail.com>
2022-11-11 16:47:14 +0530

    video-04

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom"
-import data from "../data"
+// import data from "../data"
+import axios from 'axios'
 
 function HomeScreen() {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            // dispatch({ type: 'FETCH_REQUEST' });
+            // try {
+            const result = await axios.get('/api/products');
+            //     dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
+            // } catch (err) {
+            //     dispatch({ type: 'FETCH_FAIL', payload: err.message })
+            // }
+
+            setProducts(result.data)
+        };
+        fetchData();
+    }, []);
+
     return <div>
         <h1>Featured product</h1>
         <div className='products'>
-            {data.products.map(product => (
+            {products.map(product => (
                 < div className='product' key={product.slug}>
                     <Link to={`/product/${product.slug}`}>
                         <img src={product.image} alt={product.name} />
