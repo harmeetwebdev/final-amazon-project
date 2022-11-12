2e8143819c333cab3b1a472c3b5bb99887b72df5
Harmeet Bansal <harmeets627@gmail.com>
2022-11-12 22:27:37 +0530

    video-error-message

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,7 +4,8 @@ import axios from 'axios'
 import { Col, Row } from "react-bootstrap";
 import Product from '../components/Product';
 import { Helmet } from 'react-helmet-async'
-
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox ";
 const reducer = (state, action) => {
     switch (action.type) {
         case 'FETCH_REQUEST':
@@ -19,7 +20,7 @@ const reducer = (state, action) => {
 }
 
 function HomeScreen() {
-    const [{ products }, dispatch] = useReducer(reducer, {
+    const [{ loading, error, products }, dispatch] = useReducer(reducer, {
         products: [],
         loading: true,
         error: '',
@@ -40,22 +41,28 @@ function HomeScreen() {
         fetchData();
     }, []);
 
-    return <div>
-    <Helmet>
-        <title>Amazone</title>
-    </Helmet>
-        <h1>Featured product</h1>
-        <div className='products'>
-
-            <Row>
-                {products.map(product => (
-                    <Col key={product.slug} sm={6} md={4} lg={3} className='mb-3'>
-                        <Product product={product}></Product>
-                    </Col>
-                ))}
-            </Row>
-
+    return (
+        <div>
+            <Helmet>
+                <title>Amazona</title>
+            </Helmet>
+            <h1>Featured Products</h1>
+            <div className="products">
+                {loading ? (
+                    <LoadingBox />
+                ) : error ? (
+                    <MessageBox variant="danger">{error}</MessageBox>
+                ) : (
+                    <Row>
+                        {products.map((product) => (
+                            <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                                <Product product={product}></Product>
+                            </Col>
+                        ))}
+                    </Row>
+                )}
+            </div>
         </div>
-    </div>
+    )
 }
 export default HomeScreen
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -4,6 +4,9 @@ import { Col, ListGroup, Row, Card, Badge, Button } from "react-bootstrap";
 import { useParams } from "react-router-dom"
 import Rating from "../components/Rating";
 import { Helmet } from 'react-helmet-async'
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox ";
+import { getError } from "../utils";
 
 
 const reducer = (state, action) => {
@@ -35,7 +38,7 @@ function ProductScreen() {
                 const result = await axios.get(`/api/products/slug/${slug}`);
                 dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
             } catch (err) {
-                dispatch({ type: 'FETCH_FAIL', payload: err.message })
+                dispatch({ type: 'FETCH_FAIL', payload: getError(err) })
             }
 
             // setProducts(result.data)
@@ -44,9 +47,9 @@ function ProductScreen() {
     }, [slug]);
 
     return loading ? (
-        <div>Loading...</div>
+        <LoadingBox />
     ) : error ? (
-        <div>{error}</div>
+        <MessageBox variant="danger">{error}</MessageBox>
     ) : (
         <div>
             <Row>
