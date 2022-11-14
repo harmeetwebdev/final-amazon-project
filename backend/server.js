9c267fd46003a46badb7a0d016dc79fbc4807598
Harmeet Bansal <harmeets627@gmail.com>
2022-11-14 12:50:58 +0530

    video-cartFunctionality

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,15 @@ app.get('/api/products/slug/:slug', (req, res) => {
     }
 
 })
+app.get('/api/products/:_id', (req, res) => {
+    const product = data.products.find(x => x._id === req.params._id)
+    if (product) {
+        res.send(product)
+    } else {
+        res.status(404).send({ message: 'Product Not Found' })
+    }
+
+})
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`server at http://localhost:${port}`);
