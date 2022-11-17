49c3188818185a6dceed69b601eb0951158564d9
Harmeet Bansal <harmeets627@gmail.com>
2022-11-17 23:22:49 +0530

    video-usermodel-bcrypt

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs';
 const data = {
+    users: [
+        {
+            name: 'prince',
+            email: 'admin@example.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: true,
+        },
+        {
+            name: 'preet',
+            email: 'user@example.com',
+            password: bcrypt.hashSync('123456'),
+            isAdmin: false,
+        },
+    ],
     products: [
         {
             // _id: '1',
